90ca824a6dd5d1d948f30057a7e18ff3592c143b
Jason Lester <jlester@wcs.k12.va.us>
2016-04-25 12:48:41 -0400

    Initial version that reads a PowerSchool AutoSend and writes it out in the format Destiny wants.

---
+++ sections.rb
@@ -0,0 +1,15 @@
+require 'csv'
+
+# Delete the old output file if it exists
+File.delete("output.csv") if File::exists?("output.csv")
+
+# Loop through sections file and chop out the period expression to just the period number
+CSV.foreach('sections.csv') do |row|
+  expression = row[5]
+  period = expression[0]
+
+  # Write out the output file with the updated period
+  CSV.open("output.csv", "a", {:force_quotes=>true}) do |csv|
+    csv << [row[0],row[1],row[2],row[3],row[4],period,row[6],row[7]]
+  end
+end
