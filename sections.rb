746b76d00bddef6ea34c159ad5a57ac4f265a932
Jason Lester <jlester@wcs.k12.va.us>
2016-08-03 07:49:08 -0400

    Added fields for class start and end dates.  Also, added some comments

--- sections.rb
+++ sections.rb
@@ -9,10 +9,11 @@ File.delete("output.csv") if File::exists?("output.csv")
 CSV.foreach('sections.csv') do |row|
   expression = row[5]
   period = expression[0]
+  # Create section ID by concatenating course and section numbers
   section = row[2] + "-" + row[3]
 
   # Write out the output file with the updated period
   CSV.open("output.csv", "a", {:force_quotes=>true}) do |csv|
-    csv << [row[0],row[1],row[2],section,row[4],period,row[6],row[7]]
+    csv << [row[0],row[1],row[2],section,row[4],period,row[6],row[7],row[8],row[9]]
   end
 end
